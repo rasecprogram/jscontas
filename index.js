2d4fef0c560428f50b697bbec9af93b70fb30d90
rasecprogram <156724062+rasecprogram@users.noreply.github.com>
2024-03-17 22:50:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+const numerosDaSorte = [37, 14, 26, 5, 94, 87];
+
+for (let numero of numerosDaSorte) {
+    if (numero % 2 === 0 && numero < 50) {
+        console.log(`${numero} é par e menor que 50`);
+    } else if (numero < 50) {
+        console.log(`${numero} é menor que 50`);
+    } else {
+        console.log(`${numero} é maior que 50`);
+    }
+}
